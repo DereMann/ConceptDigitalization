6505c64d9f2be94675f5485253151daadf288a40
DereMann <powderhill@gmx.at>
2019-03-18 10:14:28 +0100

    added

---
+++ Folder Wege zur Digitalen Produktion.mm
@@ -0,0 +1,214 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1552644341003" ID="ID_1208019222" MODIFIED="1552644781251" TEXT="Folder Wege zur Digitalen Produktion">
+<node CREATED="1552644354726" ID="ID_1146158591" MODIFIED="1552644601813" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Interessent geht vorbei / bekommt Folder in die Hand..<br />Ziel: Aufschlagen auf erster Seite
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552646427871" ID="ID_1861022008" MODIFIED="1552646435123" TEXT="Annahmen">
+<node CREATED="1552644692262" ID="ID_556226120" MODIFIED="1552646391745" TEXT="Aufmerksamkeitsspanne: &lt; 2 Sekunden"/>
+</node>
+<node CREATED="1552645442843" ID="ID_826976022" MODIFIED="1552646962260">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Warum schl&#228;gt er erste Seite auf?
+    </p>
+    <p>
+      Warum wird er &quot;<b>aktiviert</b>&quot;?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552644429133" ID="ID_1209471537" MODIFIED="1552645462427" TEXT="Interessante Headline"/>
+<node CREATED="1552645463000" ID="ID_298386476" MODIFIED="1552645473206" TEXT="Interessantes Bildmaterial"/>
+<node CREATED="1552645526209" ID="ID_457824435" MODIFIED="1552645530412" TEXT="Interessante Aufmachung"/>
+<node CREATED="1552645494737" ID="ID_842545703" MODIFIED="1552645501161" TEXT="aus Anstand bei direkter Anrede"/>
+<node CREATED="1552645474104" ID="ID_1975822907" MODIFIED="1552645486425" TEXT="Langeweile"/>
+</node>
+<node CREATED="1552644445092" ID="ID_1656422605" MODIFIED="1552644546164">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Wie muss der Folder wirken?<br />Welche Emotion l&#246;st er aus?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552646978316" ID="ID_696763961" MODIFIED="1552647010823" TEXT="Professionalit&#xe4;t vs Startup?"/>
+<node CREATED="1552646989685" ID="ID_1802702728" MODIFIED="1552647057447" TEXT="Aufregend vs. beruhigend (&quot;Verl&#xe4;sslichkeit&quot;)"/>
+</node>
+</node>
+<node CREATED="1552644606175" ID="ID_1396048848" MODIFIED="1552650691178" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Interessent schlagt erste Seite auf
+    </p>
+    <p>
+      Ziel:
+    </p>
+    <ul>
+      <li>
+        <p>
+          Kontaktaufnahme mit GAMED
+        </p>
+      </li>
+      <li>
+        Weiterbl&#228;ttern
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552646438357" ID="ID_1351267155" MODIFIED="1552646441495" TEXT="Annahmen">
+<node CREATED="1552644723274" ID="ID_1411694454" MODIFIED="1552646445237" TEXT="Erste Aufmerksamkeitsspanne: 4 Sekunden"/>
+<node CREATED="1552646456685" ID="ID_909491614" MODIFIED="1552646510395">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Kein Lesen von Textbl&#246;cken
+    </p>
+    <p>
+      Blick wechselt oftmals zwischen Ankern wie &#220;berschriften, Bildern, Bulletpoints etc.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1552645570733" ID="ID_1065112567" MODIFIED="1552646269320">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Warum kontaktiert Interessent GAMED?
+    </p>
+    <p>
+      Warum bl&#228;ttert Interessent weiter?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552645584944" ID="ID_1364053250" MODIFIED="1552645947451" TEXT="Interesse an Digitalisierung Produktion">
+<node CREATED="1552645997090" ID="ID_1327680678" MODIFIED="1552646008424" TEXT="Generelles Interesse / Findet Digitalisierung Cool"/>
+<node CREATED="1552646008883" ID="ID_687288369" MODIFIED="1552646060265" TEXT="Erkennt Potential zur Kostenreduzierung in Produktion"/>
+<node CREATED="1552646060794" ID="ID_275549174" MODIFIED="1552646077613" TEXT="Hat PainPoints in aktuellen Digitalisierungsvorhaben"/>
+</node>
+<node CREATED="1552645947992" ID="ID_890184866" MODIFIED="1552646630879" TEXT="Sein Interesse findet &#xdc;berschneidung mit Inhalt"/>
+<node CREATED="1552647178685" ID="ID_1818077623" MODIFIED="1552647351567">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      K&#246;nnen wir Interessent &quot;aktvieren&quot;?
+    </p>
+    <p>
+      Gef&#252;hl &quot;sofort&quot; zuschlagen zu wollen
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552647200983" ID="ID_427473679" MODIFIED="1552647216785" TEXT="Klarmachen Notwendigkeit Digitalisierung"/>
+<node CREATED="1552647219685" ID="ID_193936195" MODIFIED="1552647233905" TEXT="f&#xfc;r Aha-Effekt sorgen">
+<node CREATED="1552647241916" ID="ID_989396699" MODIFIED="1552647250525" TEXT="bzgl. Inhalt Digitalisierung"/>
+<node CREATED="1552647251024" ID="ID_371836397" MODIFIED="1552647267854" TEXT="bzgl. GAMED als super Partner"/>
+</node>
+<node CREATED="1552647295941" ID="ID_142486045" MODIFIED="1552647366677" TEXT="Pers&#xf6;nliche &#xdc;berzeugung auf Messen"/>
+</node>
+</node>
+<node CREATED="1552644655963" ID="ID_1049310708" MODIFIED="1552646647749">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Was bedeutet &#220;berschneidung Interesse / Content?
+    </p>
+    <p>
+      Welchen Content muss Interessent auf erster Seite verstehen?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552644798166" ID="ID_724831117" MODIFIED="1552644884204" TEXT="Ziel der Digitalen Produktion"/>
+<node CREATED="1552644810458" ID="ID_1413091590" MODIFIED="1552644813433" TEXT="Key Benefits"/>
+<node CREATED="1552646104172" ID="ID_1070885510" MODIFIED="1552646669693" TEXT="Kompetenz und L&#xf6;sungsf&#xe4;higkeit GAMED">
+<node CREATED="1552650387608" ID="ID_764223987" MODIFIED="1552650403817" TEXT="Kompetente Darstellung Inhalte">
+<node CREATED="1552650440976" ID="ID_898678745" MODIFIED="1552650452891" TEXT="Technologien (alt bis neu)"/>
+<node CREATED="1552650523436" ID="ID_1483564202" MODIFIED="1552650539132" TEXT="Benefits/Paint Points"/>
+</node>
+<node CREATED="1552650404196" ID="ID_1098582506" MODIFIED="1552650407178" TEXT="Referenzen"/>
+</node>
+<node CREATED="1552644814115" ID="ID_650152302" MODIFIED="1552646102949" TEXT="Hauptbausteine Vorgehensweise"/>
+</node>
+<node CREATED="1552644964069" ID="ID_1519241765" MODIFIED="1552646143647" TEXT="Wie muss sich Interessent f&#xfc;hlen?">
+<node CREATED="1552644973941" ID="ID_237495191" MODIFIED="1552645035714" TEXT="er muss das Gef&#xfc;hl habe, den Folder mit Leichtigkeit zu verstehen"/>
+<node CREATED="1552645036244" ID="ID_92683167" MODIFIED="1552645061422" TEXT="er muss das Gef&#xfc;hl haben, dass Digitalisierung einfach geht"/>
+<node CREATED="1552645062536" ID="ID_1784786289" MODIFIED="1552645074143" TEXT="er muss das Gef&#xfc;hl haben, dass GAMED der richtige Partner ist"/>
+</node>
+<node CREATED="1552644908627" ID="ID_400674567" MODIFIED="1552645103734" TEXT="Wie wecken wir tiefgreifendes Interesse?">
+<node CREATED="1552644931773" ID="ID_46418121" MODIFIED="1552645109950" TEXT="Interessanter Content"/>
+<node CREATED="1552645110662" ID="ID_1135701006" MODIFIED="1552645118267" TEXT="Interessante/Moderne Aufmachung"/>
+<node CREATED="1552646601184" ID="ID_1712730469" MODIFIED="1552646609086" TEXT="Abwechslungsreiche Aufmachung"/>
+</node>
+<node CREATED="1552645162686" ID="ID_703306056" MODIFIED="1552645172647" TEXT="Wie kann Interessent Kontakt mit GAMED aufnehmen?">
+<node CREATED="1552645179599" ID="ID_450769176" MODIFIED="1552650578331" TEXT="Telefonnummer/Anschrift einfach auffindbar"/>
+</node>
+</node>
+<node CREATED="1552645257092" ID="ID_1778272122" MODIFIED="1552645283830" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Interessent bl&#228;ttert weiter
+    </p>
+    <p>
+      Ziel: Kontaktaufnahme GAMED
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1552646810077" ID="ID_167672108" MODIFIED="1552646819063" TEXT="Welche &quot;Lesetypen gibt es?">
+<node CREATED="1552646895845" ID="ID_1923728364" MODIFIED="1552650612759" TEXT="Hektische Hin&amp;Her Springer">
+<node CREATED="1552646910363" ID="ID_1228356300" MODIFIED="1552661168066" TEXT="Aufmersamkeit: ca. 60 Sekunden?">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1552650617142" ID="ID_1883039525" MODIFIED="1552650635019" TEXT="Wichtig: verschiedene Ankerpunkte/interessante Aufmachung"/>
+</node>
+<node CREATED="1552646822277" ID="ID_1854446116" MODIFIED="1552646836552" TEXT="&quot;Von Oben Nach Unten&quot; Leser">
+<node CREATED="1552646839122" ID="ID_1048163379" MODIFIED="1552650604810" TEXT="Nimmt sich Zeit: 3 Minuten?"/>
+<node CREATED="1552646854954" ID="ID_1337057147" MODIFIED="1552646867327" TEXT="Flie&#xdf;text muss interessant sein"/>
+</node>
+</node>
+<node CREATED="1552650666092" ID="ID_304750535" MODIFIED="1552650676083" TEXT="Warum kontaktiert Interessant GAMED?"/>
+</node>
+</node>
+</map>
